ee936013b9bceceba70cc16b24341193acc786e2
TobyWow <574246986@qq.com>
2020-12-08 13:40:18 +1100

    add eat lunch

--- Day_one/src/com/uts/Text.java
+++ Day_one/src/com/uts/Text.java
@@ -5,6 +5,7 @@ public class Text {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		System.out.println("eat lunch");
 	}
 
 }
